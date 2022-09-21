c2141968cf93e281fa6264edd45373d898daa946
Bob <heycryptobob@gmail.com>
2022-09-21 18:02:48 +0300

    Create Lock

--- contracts/contracts/Vault.sol
+++ contracts/contracts/Vault.sol
@@ -5,18 +5,57 @@ pragma solidity ^0.8.9;
 import "hardhat/console.sol";
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/interfaces/IERC20.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
-contract Vault is Ownable {
+contract Vault is Ownable, Pausable {
+    using SafeERC20 for IERC20;
+
     constructor() Ownable() {}
 
     struct Lock {
-        uint256 id;
+        uint id;
         address token;
         address owner;
         uint256 amount;
         uint unlockTime;
-        string description;
     }
 
-    Lock[] private locks;
+    Lock[] private _locks;
+
+    // PUBLIC SETTERS
+
+    function lock(
+        address _token,
+        address _owner,
+        uint256 _amount,
+        uint _unlockTime
+    ) external returns (uint) {
+        return _createLock(_token, _owner, _amount, _unlockTime);
+    }
+
+    // PUBLIC GETTERS
+
+    function getLockAt(uint _lockId) public view returns (Lock memory) {
+        return _locks[_lockId];
+    }
+
+    function getTotalLockCount() public view returns (uint) {
+        return _locks.length;
+    }
+
+    // PRIVATE SETTERS
+
+    function _createLock(
+        address _token,
+        address _owner,
+        uint256 _amount,
+        uint _unlockTime
+    ) private returns (uint) {
+        uint id = _locks.length;
+        Lock memory newLock = Lock(id, _token, _owner, _amount, _unlockTime);
+        _locks.push(newLock);
+        return id;
+    }
 }
--- contracts/test/Vault.ts
+++ contracts/test/Vault.ts
@@ -2,23 +2,91 @@ import { time, loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
 import { expect } from "chai";
 import { ethers } from "hardhat";
-import deployToken from "./helpers/deployToken";
 import eth from "./helpers/eth";
 
-
-
 describe("Vault", function () {
+    const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
+    const TEN_THOUSAND_ETHER = eth(10000);
+
+    async function deployTokenFixture() {
+        const Token = await ethers.getContractFactory("StandardToken")
+        const token = await Token.deploy(TEN_THOUSAND_ETHER)
+        return token
+    }
 
     async function deployVaultFixture() {
+        const [owner, otherAccount] = await ethers.getSigners()
         const Vault = await ethers.getContractFactory("Vault")
         const vault = await Vault.deploy()
-        return vault
+        return { vault, owner, otherAccount }
+    }
+
+    async function defaultLock() {
+        const token = await loadFixture(deployTokenFixture);
+        const unlockTime = (await time.latest()) + ONE_YEAR_IN_SECS;
+        const amount = TEN_THOUSAND_ETHER;
+        return { token, unlockTime, amount };
+    }
+
+    async function deployVaultWithLockFixture() {
+        const { vault, owner, otherAccount } = await loadFixture(deployVaultFixture);
+        const { token , unlockTime, amount } = await defaultLock();
+        await token.approve(vault.address, amount);
+        const lockId = await vault.callStatic.lock(token.address, owner.address, amount, unlockTime)
+        await vault.lock(token.address, owner.address, amount, unlockTime)
+        return { vault, owner, otherAccount, token, unlockTime, amount, lockId }
     }
 
     describe("Deployment", function () {
-        it("Should deploy", async function () {
-            const vault = await loadFixture(deployVaultFixture);
-            expect(vault).to.haveOwnProperty("address");
+        it("Should set the right owner", async function () {
+            const { vault, owner } = await loadFixture(deployVaultFixture);
+            expect(await vault.owner()).to.equal(owner.address);
+        })
+    })
+
+    describe("Lock", function () {
+
+        describe("Creation", function () {
+            it("Should set the right token", async function () {
+                const { vault, token, lockId } = await loadFixture(deployVaultWithLockFixture)
+                const lock = await vault.getLockAt(lockId)
+                expect(await lock.token).to.equal(token.address);
+            })
+            
+            it("Should set the right owner", async function () {
+                const { vault, owner, lockId } = await loadFixture(deployVaultWithLockFixture)
+                const lock = await vault.getLockAt(lockId)
+                expect(await lock.owner).to.equal(owner.address);
+            });
+            
+            it("Should set the right amount", async function () {
+                const { vault, amount, lockId } = await loadFixture(deployVaultWithLockFixture)
+                const lock = await vault.getLockAt(lockId)
+                expect(await lock.amount).to.equal(amount);
+            });
+
+            it("Should set the right unlock time", async function () {
+                const { vault, unlockTime, lockId } = await loadFixture(deployVaultWithLockFixture)
+                const lock = await vault.getLockAt(lockId)
+                expect(await lock.unlockTime).to.equal(unlockTime);
+            });
+        })
+
+        describe("Access", function () {
+            it("Should get total lock count", async function () {
+                const { vault } = await loadFixture(deployVaultWithLockFixture)
+                expect(await vault.getTotalLockCount()).to.equal(1)
+            })
+
+            it("Should get lock at index", async function () {
+                const { vault, lockId, token, owner, amount, unlockTime } = await loadFixture(deployVaultWithLockFixture)
+                const lock = await vault.getLockAt(lockId)
+                expect(lock.id).to.equal(lockId)
+                expect(lock.token).to.equal(token.address)
+                expect(lock.owner).to.equal(owner.address)
+                expect(lock.amount).to.equal(amount)
+                expect(lock.unlockTime).to.equal(unlockTime)
+            })
         })
     })
 })
