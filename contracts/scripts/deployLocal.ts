74199066cfe00ffce234b3998ff941ce50acb31a
Bob <heycryptobob@gmail.com>
2022-09-27 16:53:22 +0300

    dummy test data

--- contracts/scripts/deployLocal.ts
+++ contracts/scripts/deployLocal.ts
@@ -2,10 +2,12 @@ import { ethernal, ethers } from "hardhat";
 import { time } from "@nomicfoundation/hardhat-network-helpers";
 import { BigNumber, Contract } from "ethers";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { faker } from '@faker-js/faker';
 
 ethernal.resetWorkspace("Vault")
 
 const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
+const ONE_YEAR_IN_MINS = 365 * 24 * 60;
 
 async function deployContract(name: string, ...args: any) {
     const Contract = await ethers.getContractFactory(name);
@@ -20,23 +22,41 @@ async function deployContract(name: string, ...args: any) {
     return contract;
 }
 
-async function createLock(vault: Contract, token: Contract, owner: SignerWithAddress, amount: BigNumber) {
-    const unlockTime = (await time.latest()) + ONE_YEAR_IN_SECS;
-    await token.approve(vault.address, amount);
-    await vault.deposit(token.address, owner.address, amount, unlockTime)
+async function createLock(vault: Contract, token: Contract, wallet: SignerWithAddress, unlockTime: number, amount: BigNumber) {
+    unlockTime = (await time.latest()) + unlockTime;
+    await token.connect(wallet).approve(vault.address, amount);
+    await vault.deposit(token.address, wallet.address, amount, unlockTime)
+}
+
+function getRandomNumberBetween(min: number, max: number) {
+    return Math.floor(Math.random()*(max-min+1)+min);
+}
+
+async function randomLock(vault: Contract, token: Contract, owner: SignerWithAddress, wallet: SignerWithAddress, unlockTime: number, amount: BigNumber) {
+    await token.transfer(wallet.address, amount)
+    await createLock(vault, token, wallet, unlockTime, amount)
 }
 
 async function main() {
 
     const vault = await deployContract("Vault")
-    const [owner] = await ethers.getSigners()
+    const signers = await ethers.getSigners()
+    const [owner] = signers
 
-    const n = 12;
+    const n = 15;
     for (let i = 0; i < n; i++) {
-        const token = await deployContract("StandardToken", ethers.utils.parseEther("10000"), `Token ${i}`, `ST${i}`),
-        const amount = (await token.totalSupply()).div(n)
-        for (let j = 0; j < n; j++) {
-            await createLock(vault, token, owner, amount)
+        const { name, symbol, atomicNumber} = faker.science.chemicalElement()
+        // console.log(name, symbol, atomicNumber)
+        const totalSupply = atomicNumber * 100
+        const token = await deployContract("StandardToken", ethers.utils.parseEther(totalSupply.toString()), name, symbol.toUpperCase())
+
+        const m = getRandomNumberBetween(1, signers.length - 1)
+        for (let j = 0; j < m; j++) {
+            const wallet = signers[j]
+            const amountNb = getRandomNumberBetween(1, 5) * totalSupply / 100
+            const amountBn = ethers.utils.parseEther(amountNb.toString())
+            const unlockTime = getRandomNumberBetween(1, 60) * ONE_YEAR_IN_MINS
+            randomLock(vault, token, owner, wallet, unlockTime, amountBn)
         }
     }
 
