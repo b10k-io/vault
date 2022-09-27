164dea889e622d9859be44cc375f0faf3b875576
Bob <heycryptobob@gmail.com>
2022-09-27 15:59:13 +0300

    remove console.log

--- src/components/pages/Dashboard.tsx
+++ src/components/pages/Dashboard.tsx
@@ -2,9 +2,6 @@ import useAuth from "../helpers/useAuth"
 import { Navigate } from "react-router-dom"
 
 const Dashboard = () => {
-
-    console.log("useAuth()", useAuth())
-
     if (useAuth()) {
         return (
             <h1>Dashboard</h1>
@@ -12,8 +9,6 @@ const Dashboard = () => {
     } else {
         return <Navigate to="/" />
     }
-
-
 }
 
 export default Dashboard
--- src/hooks/useLockTable.ts
+++ src/hooks/useLockTable.ts
@@ -3,7 +3,6 @@ import { useLockIdsByToken, useLockByIds } from "./Vault"
 
 export function useLockTable(address: string, tokenAddress: string): ILock[] | undefined {
     const lockIds = useLockIdsByToken(address, tokenAddress)
-    console.log("lockIds", lockIds)
     const locks: ILock[] | undefined = useLockByIds(address, lockIds)
     return locks
 }
